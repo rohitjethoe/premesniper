11543d482732b9379858ca3b81c3dcc8ed1f6ae9
rohitjethoe <rohitchai@icloud.com>
2020-08-29 15:38:55 +0200

    Server setup.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const App = require('./server/main');
+const express = require('express');
+const cors = require('cors');
 const app = require('./server/main');
 
-(async() => {
-    await app.openBrowser();
-    await app.findItem('Tops/Sweaters', 'Static Sweater ');
-    await app.launchPage();
-    await app.addToCart('Black');
-})()
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use('/api', require('./server/routes/web'));
+
+const PORT = 5000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`DEV | Server has been launched on http://localhost:${PORT}`));
+
+// Debugging code ---
+//
+// (async() => {
+//     await app.openBrowser();
+//     await app.findItem('Tops/Sweaters', 'Static Sweater ');
+//     await app.launchPage();
+//     await app.addToCart('Black');
+// })() 
