ae4755d6d4d5bb143aa80c0d9ea8abf512f8a7d6
rohitjethoe <rohitchai@icloud.com>
2020-09-01 20:33:46 +0200

    Uncommented server and commented debugging code.

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
 const app = require('./server/main');
 
-const server = express();
+// const server = express();
 
-server.use(cors());
-server.use(express.json());
-server.use('/api', require('./server/routes/web'));
+// server.use(cors());
+// server.use(express.json());
+// server.use('/api', require('./server/routes/web'));
 
-const PORT = 5000 || process.env.PORT;
+// const PORT = 5000 || process.env.PORT;
 
-server.listen(PORT, () => console.log(`DEV | Server has been launched on http://localhost:${PORT}`));
+// server.listen(PORT, () => console.log(`DEV | Server has been launched on http://localhost:${PORT}`));
 
 // Debugging code ---
 //
-// (async() => {
-//     await app.openBrowser();
-//     await app.findItem('Tops/Sweaters', 'Static Sweater ');
-//     await app.launchPage();
-//     await app.addToCart('Black');
-// })() 
+(async() => {
+    const params = {
+        name: 'Rohit Jethoe',
+        email: 'test@gmail.com',
+        phone: '0600000000',
+        address: 'Grote Houtstraat 12',
+        city: 'Haarlem',
+        zip: '1992LV',
+        country_code: 'NL'
+    }
+    await app.openBrowser();
+    await app.findItem('Tops/Sweaters', 'Static Sweater ');
+    await app.launchPage();
+    await app.addToCart('Black');
+    await app.fillFormData(params);
+})() 
