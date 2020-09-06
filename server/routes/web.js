c8fd4a8f378e926eca11cdd094e63f35bc579975
rohitjethoe <rohitchai@icloud.com>
2020-09-06 11:34:01 +0200

    New controller directory added.

--- server/routes/web.js
+++ server/routes/web.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const controller = require('../controller/main.js')
 // Root directory.
 router.get('/', (req, res) => res.send({ response: res.statusCode }));
 
